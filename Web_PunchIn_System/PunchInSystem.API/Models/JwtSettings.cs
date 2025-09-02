7b5c865b4f1797bd0a6df2e6eb13492596836a10
shubh patel <115077008+shubhpatel269@users.noreply.github.com>
2025-09-02 12:45:26 +0530

    de

--- Web_PunchIn_System/PunchInSystem.API/Models/JwtSettings.cs
+++
@@ -1,9 +0,0 @@
-namespace PunchInSystem.API.Models;
-
-public class JwtSettings
-{
-    public string Secret { get; set; } = null!;
-    public int TokenExpirationInMinutes { get; set; }
-    public string Issuer { get; set; } = "PunchInSystem.API";
-    public string Audience { get; set; } = "PunchInSystem.Clients";
-}
