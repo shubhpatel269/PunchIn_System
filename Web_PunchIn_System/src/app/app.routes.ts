3dca1db06108cb715edf5f33b9999ce4abed1756
shubh patel <115077008+shubhpatel269@users.noreply.github.com>
2025-09-04 22:43:49 +0530

    AuthGuard
    created a separate authguard for admin and employee.. nd remove old punch in.. and enhanced routes

--- Web_PunchIn_System/src/app/app.routes.ts
+++ Web_PunchIn_System/src/app/app.routes.ts
@@ -5,26 +5,32 @@ import { Home } from './features/home/home';
 import { Login } from './features/login/login';
 import { ManageDesignation } from './features/manage-designation/manage-designation';
 import { ManageEmployee } from './features/manage-employee/manage-employee';
-import { PunchIn } from './features/punch-in/punch-in';
 import { EmployeeLayoutComponent } from './features/employee-layout/employee-layout.component';
 import { EmployeeDashboardComponent } from './features/employee-dashboard/employee-dashboard.component';
 import { EmployeeProfileComponent } from './features/employee-profile/employee-profile.component';
 import { EmployeeAttendanceComponent } from './features/employee-attendance/employee-attendance.component';
 import ManageCompanyAdmin from './features/manage-company-admin/manage-company-admin';
-import ManageCompany from './features/company-profile/company-profile';
 import { Landing } from './features/landing/landing';
 import { CompanyRegister } from './features/company-register/company-register';
 import { NotFound } from './not-found/not-found';
 import ManageCompanyProfile from './features/company-profile/company-profile';
+import { AddAdmin } from './features/add-admin/add-admin';
+import { AddUser } from './features/add-user/add-user';
+import { AdminGuard } from './shared/guards/admin.guard';
+import { EmployeeGuard } from './shared/guards/employee.guard';
 
 export const routes: Routes = [
     { path: '', component: Landing },
     { path: 'register', component: CompanyRegister },
     { path: 'login', component: Login },
-    { path: 'punchin', component: PunchIn },
     {
-        path: 'admin', component: AdminDashboard, children: [
+        path: 'admin', 
+        component: AdminDashboard, 
+        canActivate: [AdminGuard],
+        children: [
             { path: 'dashboard', component: AttendanceDashboardComponent },
+            { path: 'add-admin', component: AddAdmin },
+            { path: 'add-user', component: AddUser },
             { path: 'manage-user', component: ManageEmployee },
             { path: 'manage-designation', component: ManageDesignation },
             { path: 'manage-company-admin', component: ManageCompanyAdmin },
@@ -35,7 +41,11 @@ export const routes: Routes = [
         ]
     },
     {
-        path: 'employee', component: EmployeeLayoutComponent, children: [
+        path: 'employee', 
+        component: EmployeeLayoutComponent, 
+        canActivate: [EmployeeGuard],
+        children: [
+            { path: 'home', component: Home },
             { path: 'dashboard', component: EmployeeDashboardComponent },
             { path: 'profile', component: EmployeeProfileComponent },
             { path: 'attendance', component: EmployeeAttendanceComponent },
